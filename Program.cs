5b19b93ed757535c4e44015107e203efcecf5648
Arnyev <artur.niewiadomski.93@gmail.com>
2020-07-10 12:53:35 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Reddit;
+
+namespace reddit_scraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appid = string.Empty;
+            string refreshToken = string.Empty;
+            var reddit = new RedditClient(appid, refreshToken);
+        }
+    }
+}
